b5e5920f790f12927ffe284a80c64ac941c943d5
Will <sylus1984@gmail.com>
2013-10-16 01:01:16 -0400

    Fix commenting for panels layouts.

--- plugins/layouts/laurier/laurier.tpl.php
+++ plugins/layouts/laurier/laurier.tpl.php
@@ -1,31 +1,21 @@
 <?php
 /**
  * @file
- * Adaptivetheme implementation to present a Panels layout.
+ * Implementation to present a Panels based layout.
  *
  * Available variables:
  * - $content: An array of content, each item in the array is keyed to one
  *   panel of the layout.
  * - $css_id: unique id if present.
  * - $panel_prefix: prints a wrapper when this template is used in a context,
- *   such as when rendered by Display Suite or other module - the wrapper is
- *   added by Adaptivetheme in the appropriate process function.
+ *   such as when rendered by Display Suite or other module.
  * - $panel_suffix: closing element for the $prefix.
- *
- * @see adaptivetheme_preprocess_berton()
- * @see adaptivetheme_preprocess_node()
- * @see adaptivetheme_process_node()
  */
-
-// Ensure variables are always set. In the last hours before cutting a stable
-// release I found these are not set when in a Field Collection using Display
-// Suite, even though they are initialized in the templates preprocess function.
-// This is a workaround, that may or may not go away.
 $panel_prefix = isset($panel_prefix) ? $panel_prefix : '';
 $panel_suffix = isset($panel_suffix) ? $panel_suffix : '';
 ?>
 <?php print $panel_prefix; ?>
-<div class="at-panel panel-display laurier clearfix" <?php if (!empty($css_id)): print "id=\"$css_id\""; endif; ?>>
+<div class="panel-display laurier clearfix" <?php if (!empty($css_id)): print "id=\"$css_id\""; endif; ?>>
   <?php if ($content['laurier_top']): ?>
   <div class="region-laurier-top span-8">
       <?php print $content['laurier_top']; ?>
