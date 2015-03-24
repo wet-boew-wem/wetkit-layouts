3a17eb196e47ae94807f49f309585b272e50ef61
William Hearn <sylus1984@gmail.com>
2015-03-24 18:24:27 -0400

    Issue #2453387: missing submenu for dashboard in 4x

--- plugins/layouts/boxton/boxton.tpl.php
+++ plugins/layouts/boxton/boxton.tpl.php
@@ -12,7 +12,7 @@
 
 <div class="panel-display boxton clearfix <?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 
-  <div class="boxton-container boxton-content boxton-content-region panel-panel">
+  <div class="boxton-container boxton-content boxton-content-region panel-panel col-md-12">
     <?php print $content['contentmain']; ?>
   </div>
 
