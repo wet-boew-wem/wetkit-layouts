9508a0f5c202f25e4369f9227c67221d1c349dde
William Hearn <sylus1984@gmail.com>
2016-08-21 22:08:16 -0400

    Update layout to work with v4.0.22.

--- plugins/layouts/pearson/pearson.tpl.php
+++ plugins/layouts/pearson/pearson.tpl.php
@@ -34,7 +34,7 @@ $panel_suffix = isset($panel_suffix) ? $panel_suffix : '';
           </div>
         </div>
         <div class="region-pearson-second">
-          <nav role="navigation" id="wb-sec" typeof="SiteNavigationElement" class="col-md-3 col-md-pull-9 visible-md visible-lg">
+          <nav role="navigation" id="wb-sec" typeof="SiteNavigationElement" class="col-md-3 col-md-pull-9">
               <h2 id="wb-side-nav"><?php print t('Section menu'); ?></h2>
               <?php if ($content['pearson_second']): ?>
                 <div class="wb-sec-def">
